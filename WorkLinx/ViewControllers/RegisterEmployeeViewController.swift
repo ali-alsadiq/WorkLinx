0ab85d3fc0f7192ab68e661c95bd1fcaa2f0070c
Alex Wang <wza2479110@gmail.com>
2023-07-08 00:01:25 -0700

    Adding UI of
    Adding some UI into our RegisterEmployerViewController

--- WorkLinx/ViewControllers/RegisterEmployeeViewController.swift
+++ WorkLinx/ViewControllers/RegisterEmployeeViewController.swift
@@ -1,20 +1,91 @@
-//
-//  RegisterEmployeeViewController.swift
-//  WorkLinx
-//
-//  Created by Ali Alsadiq on 2023-07-04.
-//
-
 import UIKit
 
 class RegisterEmployeeViewController: UIViewController {
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        checkWorkInvitation()
+    }
+    
+    func checkWorkInvitation() {
+        // Check if the employee has any work invitations
+        let hasInvitations = checkIfEmployeeHasInvitations()
         
-        // show work invitation if employee has any
-        // allow employee to select and confirm, or reject
-        // else show a message saying no invitations under this email
-        // show home view last
+        if hasInvitations {
+            // Employee has invitations
+            showInvitationOptions()
+        } else {
+            // Employee doesn't have any invitations
+            showNoInvitationsMessage()
+        }
+    }
+    
+    func checkIfEmployeeHasInvitations() -> Bool {
+        // Logic to check if the employee has any work invitations
+        // You can fetch the invitations from your data source (e.g., database)
+        // and return true if there are invitations for the employee's email address,
+        // otherwise return false
+        
+        // Replace the below return statement with your implementation
+        
+        return false
+    }
+    
+    func showInvitationOptions() {
+        // Present a view or alert allowing the employee to select and confirm
+        // or reject the work invitation(s)
+        // Implement the necessary UI and interaction logic
+        
+        // Replace the below code with your implementation
+        
+        let alert = UIAlertController(title: "Work Invitation", message: "You have work invitation(s).", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Confirm", style: .default, handler: { _ in
+            // Handle invitation confirmation
+            self.showHomeView()
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Reject", style: .destructive, handler: { _ in
+            // Handle invitation rejection
+            self.showHomeView()
+        }))
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func showNoInvitationsMessage() {
+        // Show a message indicating that there are no work invitations for the employee
+        // Implement the necessary UI and display the message
+        
+        // Replace the below code with your implementation
+        
+        let noInvitationsLabel = UILabel()
+        noInvitationsLabel.text = "No work invitations under this email."
+        noInvitationsLabel.textAlignment = .center
+        noInvitationsLabel.numberOfLines = 0
+        
+        view.addSubview(noInvitationsLabel)
+        
+        // Add constraints to position the label
+        noInvitationsLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            noInvitationsLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            noInvitationsLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        
+        // Optionally, you can add additional styling to the label as per your app's design
+    }
+    
+    func showHomeView() {
+        // Present the home view to the employee
+        // Implement the necessary logic to navigate to the home view
+        
+        // Replace the below code with your implementation
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let homeViewController = storyboard.instantiateViewController(withIdentifier: "HomeViewController")
+        
+        navigationController?.setViewControllers([homeViewController], animated: true)
     }
 }
--- WorkLinx/ViewControllers/RegisterEmployerViewController.swift
+++ WorkLinx/ViewControllers/RegisterEmployerViewController.swift
@@ -1,16 +1,10 @@
-//
-//  RegisterEmployerViewController.swift
-//  WorkLinx
-//
-//  Created by Ali Alsadiq on 2023-07-04.
-//
-
 import UIKit
 
 class RegisterEmployerViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        createStackview()
         
         // Add nav bar
         let navigationBar = CustomNavigationBar(title: "Create Your Workspace")
@@ -26,11 +20,65 @@ class RegisterEmployerViewController: UIViewController {
             navigationBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             navigationBar.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
-        
     }
     
     @objc func goBack() {
         // Pop the current view controller from the navigation stack
         dismiss(animated: true, completion: nil)
     }
+    
+    @objc func createStackview() {
+        let stackView = UIStackView()
+        stackView.frame = view.bounds
+        stackView.axis = .vertical
+        stackView.spacing = 0
+        stackView.alignment = .fill
+        
+        let fields = ["First Name", "Last Name", "Email", "Qualified Positions"]
+        
+        for fieldText in fields {
+            let containerView = UIView()
+            containerView.backgroundColor = UIColor.systemGray.withAlphaComponent(0.2) // Adjust the alpha and color to your preference
+            containerView.layer.cornerRadius = 0 // Optional: Add corner radius for rounded corners
+            containerView.layer.borderColor = UIColor.black.cgColor // Set border color to black
+            containerView.layer.borderWidth = 1 // Set border width
+            
+            let label = UILabel()
+            label.text = fieldText
+            label.textAlignment = .left
+            label.font = .systemFont(ofSize: 15, weight: .bold)
+            label.heightAnchor.constraint(equalToConstant: 50).isActive = true
+            
+            containerView.addSubview(label)
+            label.translatesAutoresizingMaskIntoConstraints = false
+            
+            NSLayoutConstraint.activate([
+                label.topAnchor.constraint(equalTo: containerView.topAnchor),
+                label.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20), // Add left padding
+                label.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20), // Add right padding
+                label.bottomAnchor.constraint(equalTo: containerView.bottomAnchor)
+            ])
+            
+            stackView.addArrangedSubview(containerView)
+            
+            // Adjust containerView's constraints
+            containerView.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                containerView.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 0), // Align with stackView's leading edge
+                containerView.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: 0) // Align with stackView's trailing edge
+            ])
+        }
+        
+        view.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100), // Adjust the constant to move the stackView further down
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0), // Align with view's leading edge
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0) // Align with view's trailing edge
+        ])
+    }
 }
+
+
+// Hello
